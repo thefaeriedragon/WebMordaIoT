4fb1c54f8a6ae0cfe50892b90b2e1c1e0ff1f4e9
denisr <denisr@>
2016-09-09 18:14:38 +0300

    SVG SOOO Beatiful

--- static/js/mapPicker.js
+++ static/js/mapPicker.js
@@ -10,14 +10,31 @@ var svgdom;
   }
 
 itemBehavior("#Pot1",svgdom);
+labelLoader("#Pot1",svgdom);
+
 itemBehavior("#Pot2",svgdom);
+labelLoader("#Pot2",svgdom);
+
 itemBehavior("#Pot3",svgdom);
+labelLoader("#Pot3",svgdom);
+
 itemBehavior("#Pot4",svgdom);
+labelLoader("#Pot4",svgdom);
+
 itemBehavior("#Pot5",svgdom);
+labelLoader("#Pot5",svgdom);
+
 itemBehavior("#Pot6",svgdom);
+labelLoader("#Pot6",svgdom);
+
 itemBehavior("#Pot7",svgdom);
+labelLoader("#Pot7",svgdom);
+
 itemBehavior("#Pot8",svgdom);
+labelLoader("#Pot8",svgdom);
+
 itemBehavior("#Pot9",svgdom);
+labelLoader("#Pot9",svgdom);
 
 itemBehavior("#Crane4",svgdom);
 itemBehavior("#Crane3",svgdom);
@@ -25,7 +42,11 @@ itemBehavior("#Crane2",svgdom);
 itemBehavior("#Crane1",svgdom);
 
 itemBehavior("#WaterMeter",svgdom);
+waterSpeed("#WaterMeter",svgdom);
+
 itemBehavior("#WaterPool",svgdom);
+pumpPower("#WaterPool",svgdom);
+
 itemBehavior("#WaterTower",svgdom);
 
 itemBehavior("#Lamp1",svgdom);
@@ -46,6 +67,55 @@ jQuery("#Items "+item, svgdom).hover(
         idgh.appendTo("#sidebar");
   }
   );
+
+}
+
+function waterSpeed(item, svgdom){
+var ti = setInterval(function() {
+jQuery(item+"Label", svgdom).text(""+ Math.floor(Math.random()*100) +" l/s");
+ }
+, 1000);
+}
+
+
+
+function labelLoader(item, svgdom){
+
+var ti = setInterval(function() {
+jQuery("#PotLabel"+item[item.length -1]+" .PotLabelTemp", svgdom).text(""+ Math.floor(Math.random()*100) +"c");
+jQuery("#PotLabel"+item[item.length -1]+" .PotLabelHum", svgdom).text(""+ Math.floor(Math.random()*100) +"%");
+jQuery("#PotLabel"+item[item.length -1]+" .PotLabelGHum", svgdom).text(""+ Math.floor(Math.random()*100) +"%");
+ }
+, 1000);
+
+}
+
+function arrowHide(j, svgdom){
+jQuery("#Arrow"+j, svgdom).attr("visibility", "visible");
+}
+
+function pumpPower(item, svgdom){
+for (var i = 1; i < 4; i++) {
+   jQuery("#Arrow"+i, svgdom).attr("visibility", "hidden");
+}
+
+
+setTimeout(function() {
+   arrowHide(1,svgdom);
+}, 1000)
+
+setTimeout(function() {
+   arrowHide(2,svgdom);
+}, 2000)
+
+setTimeout(function() {
+   arrowHide(3,svgdom);
+}, 3000)
+
+setTimeout(function() {
+   pumpPower(item,svgdom);
+}, 4000)
+
 }
 
 //$("#imap").ready(svgMechanic);
